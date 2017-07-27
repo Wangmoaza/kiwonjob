2c9d0eba915afa15bfaf9294a9d608900df30010
Ha-Eun Hwangbo <haeun0512@hanmail.net>
2017-07-27 17:01:08 +0900

    update

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # kiwonjob
 Helping Kiwon
 
+## file description
 
+#### files from Kiwon
+
+* *files in clusters folder* : gene list of each clusters using hierarchical clustering of the heat map. c stands for case, f stands for features. mini_ files are artificial clusters made by Kiwon.
+* *breast_correlation_v2.txt* : edge information of network format src/dest/corr/p-value
+* *deep_input_c3_pert_v2LogN* : cut off 3
+* *deep_input_c3_pert_v2LogN_top* : contains only features with high variance
+* *deep_input_c3_pert_v2LogN_top_cs_3x* : plus case selection
+
+#### files made by me
+
+* *gene_essential_cnt* : gene essentiality is based on deep_input_c3_pert_v2LogN file.
+*  *breast_cancer_node_df* : node data frame of network. cluster marked, centrality calculated.
+*  
+
+## lab note by date
 #### 2017-07-21
 
 Visualized cluster c2f4 with cytoscape
@@ -15,5 +31,10 @@ Visualized cluster c2f4 with cytoscape
 
 * centrality calculation uisng networkx functions
 * data saved in file breast_cancer_node_df
+
+#### 2017-07-26
+
+* drew box plot for centralities in two groups (essential, non-essential)
+
 
 
--- network.py
+++ network.py
@@ -3,7 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def print_stat(network_df_df):
+def print_stat(network_df):
+	""" This method prints statistics of the network based on pandas DataFrame.
+	Appropriate data frame should have fields src, dest.src
+
+	args:
+		network_df (pd.DataFrame) : data frame containig network information
+
+	"""
     src_nodes = set(np.unique(network_df.src.values))
     dest_nodes = set(np.unique(network_df.dest.values))
     print "number of all nodes: " + str(len(src_nodes | dest_nodes))
@@ -12,17 +19,26 @@ def print_stat(network_df_df):
     print "number of src_nodes - dest_nodes: " + str(len(src_nodes - dest_nodes))
     print "number of dest_nodes - src_nodes: " + str(len(dest_nodes - src_nodes))
     print "number of all edges: " + str(network_df.shape[0])
+### END - print_stat
+
 
 def mark_clusters():
-    network = pd.read_table("../breast_cancer_corr_essential_marked", sep='\t', header=0, index_col=0)
-    #network['c'] = np.zeros(network.shape[0], dtype=np.int32)
+	""" Make columns that mark the clusters (c1, c2, c3, f) and their intersections (c1f1, c1f2, ...)
+	and return the new network DataFrame
 
+	returns:
+		network DataFrame containing cluster information
+
+	"""
+    network = pd.read_table("../breast_cancer_corr_essential_marked", sep='\t', header=0, index_col=0)
+
+    # mark case (c) clusters
     for c in range(1, 4):
         df_c = pd.read_table('../clusters/mini_c{0}.txt'.format(c), sep='\t', header=None, names=['gene'])
         df_c = df_c['gene'].str.split(':', expand=True)
         df_c.columns = ['gene', 'cell']
         print "df_c" + str(c) 
-        #rint df_c
+        
         
         diff_set = set(df_c.gene.values) - set(network.src.values)
         
@@ -42,12 +58,13 @@ def mark_clusters():
             if row.src in df_c.gene.values:
                 c_col[idx] = 1
         ### END - for
+
         network['c{0}'.format(c)] = c_col
-        ### END - for
     ### END - for
     
-    network['f'] = np.zeros(network.shape[0], dtype=np.int32)
-    
+    # mark feature (f) clusters
+    network['f'] = np.zeros(network.shape[0], dtype=np.int32)  # make new column
+
     for f in range(1, 6):
         df_f = pd.read_table('../clusters/mini_f{0}.txt'.format(f), sep='\t', header=None, names=['gene', 'cluster'])
         diff_set = set(df_f.gene.values) - set(network.src.values)
@@ -68,6 +85,7 @@ def mark_clusters():
         ### END - for
     ### END - for
     
+    # mark intersection of case and feature clusters
     for c in range(1, 4):
         for f in range(1, 6):
             idx = network[(network['c{0}'.format(c)] == 1) | (network['f'] == f)].index
@@ -77,18 +95,42 @@ def mark_clusters():
         ### END - for
     ### END - for
     return network
+### END - mark_clusters
+
 
 def construct_node_df(G):
+	""" Constructs node information DataFrame from edge information DataFrame.
+
+	args:
+		G (nx.DiGraph): graph corresponding to the data frame for removing the isolated nodes
+
+	returns:
+		pd.DataFrame containing nnetwork node information
+
+	"""
 	df = pd.read_table('../breast_cancer_cluster_marked_cf', sep='\t', header=0, index_col=0)
 	df.drop_duplicates(subset='src', inplace=True)
 	df.drop(['dest', 'r', 'p'], axis=1, inplace=True)
 	df.set_index('src', inplace=True)
-	# remove node that are not in the main conneted_component
+	# remove node that are not in the main connected component
 	to_be_removed = isolated_nodes(G)
 	df.drop(list(to_be_removed), axis=0, inplace=True)
 	return df
+### END - construct_node_df
 
-def construct_graph():
+
+def construct_graph(set_cluster=True):
+	""" Constructs networkx graph from network DataFrame. 
+	If set_cluster is True, cluster information is added to the nodes.
+
+	args:
+		set_cluster (bool) : whether to put cluster information to nodes
+
+	returns:
+		networkx constructed from edge information data frame
+
+	"""
+
 	# network_df should also have clustered information
 	df = pd.read_table('../breast_cancer_cluster_marked_cf', sep='\t', header=0, index_col=0)
 	ess_dic, c1_dic, c2_dic, c3_dic, f_dic = {}, {}, {}, {}, {}
@@ -96,34 +138,44 @@ def construct_graph():
 	# add edges to the graph
 	for idx, row in df.iterrows():
 		G.add_edge(row.src, row.dest, r=row.r, p=row.p)
-		ess_dic[row.src] = row.src_ess
-		c1_dic[row.src] = row.c1
-		c2_dic[row.src] = row.c2
-		c3_dic[row.src] = row.c3
-		f_dic[row.src] = row.f
+		if set_cluster:
+			ess_dic[row.src] = row.src_ess
+			c1_dic[row.src] = row.c1
+			c2_dic[row.src] = row.c2
+			c3_dic[row.src] = row.c3
+			f_dic[row.src] = row.f
 	### END - for
-
-	nx.set_node_attributes(G, 'ess', ess_dic)
-	nx.set_node_attributes(G, 'c1', c1_dic)
-	nx.set_node_attributes(G, 'c2', c2_dic)
-	nx.set_node_attributes(G, 'c3', c3_dic)
-	nx.set_node_attributes(G, 'f', f_dic)
+	if set_cluster:
+		nx.set_node_attributes(G, 'ess', ess_dic)
+		nx.set_node_attributes(G, 'c1', c1_dic)
+		nx.set_node_attributes(G, 'c2', c2_dic)
+		nx.set_node_attributes(G, 'c3', c3_dic)
+		nx.set_node_attributes(G, 'f', f_dic)
 
 	try:
 		G.remove_node(np.nan)
-		#G.remove_node('')
 	except NetworkXError:
 		print "node requested to remove does not exists"
 
 	# remove node that are not in the main conneted_component
 	to_be_removed = isolated_nodes(G)
-
 	for n in to_be_removed:
 		G.remove_node(n)
 
 	return G
+### END - construct_graph
+
 
 def isolated_nodes(G):
+	""" Returns the nodes that are not in the largest weakly connected component.
+
+	args:
+		G (nx.DiGraph) : graph
+
+	returns:
+		set of nodes that are not in the largest weakly connected component
+
+	"""
 	to_be_removed = set()
 	idx = 0
 	for comp in nx.weakly_connected_components(G):
@@ -131,9 +183,21 @@ def isolated_nodes(G):
 			to_be_removed.union(comp)
 		idx += 1
 	return to_be_removed
+### END - isolated_nodes
 
 
 def centrality(G):
+	""" Calculates the in-degree, out-degree, closeness, betweenness centrality
+	for the given graph.
+
+	args:
+		G (nx.DiGraph) : input graph
+
+	returns:
+		tuple of in-degree, out-degree, closeness, betweenness centrality dictionaries
+		that the keys are nodes.
+
+	"""
 	print "calculating in_degree centrality..."
 	in_degree = nx.in_degree_centrality(G)
 	print "calculating out_degree centrality..."
@@ -144,7 +208,7 @@ def centrality(G):
 	betweenness = nx.betweenness_centrality(G)
 
 	return in_degree, out_degree, closeness, betweenness
-
+### END - centrality
 
 def main():
 	G = construct_graph()
@@ -155,7 +219,7 @@ def main():
 	node_df['close'] = pd.Series(closeness)
 	node_df['between'] = pd.Series(betweenness)
 	node_df.to_csv('breast_cancer_node_df', sep='\t')
-
+### END - main
 
 if __name__ == "__main__":
 	main()
